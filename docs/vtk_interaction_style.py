07949c1a9afc9723a26263efb447d2b910a70327
ruben_laptop <rubendebruin@gmail.com>
2022-08-07 03:39:59 -0500

    added rubber-band display

--- docs/vtk_interaction_style.py
+++ docs/vtk_interaction_style.py
@@ -22,7 +22,8 @@ Create an interaction style using the Blender default key-bindings (with left-se
 Due to C++/Python implementation this requires re-implementing the interaction completely as the c++ methods
 can not be overridden/overloaded in python.
 
-the code below is largely a translation of https://github.com/Kitware/VTK/blob/master/Interaction/Style/vtkInteractorStyleTrackballCamera.cxx   
+Camera action code is largely a translation of https://github.com/Kitware/VTK/blob/master/Interaction/Style/vtkInteractorStyleTrackballCamera.cxx
+Rubber band code    
 
 Interaction:
 
@@ -86,6 +87,8 @@ class BlenderStyle(vtk.vtkInteractorStyleUser):
             self.Pan()
         elif self.MiddleButtonDown and Ctrl and not Shift:
             self.Zoom()  # Dolly
+        elif self.LeftButtonDown:
+            self.DrawRubberBand()
 
         self.InvokeEvent(vtk.vtkCommand.InteractionEvent, None)
 
@@ -111,8 +114,16 @@ class BlenderStyle(vtk.vtkInteractorStyleUser):
     def LeftButtonPress(self, obj, event):
         self.LeftButtonDown = True
 
+        rwi = self.GetInteractor()
+        self.start_x, self.start_y = rwi.GetEventPosition()
+
+        self.InitializeScreenDrawing()
+
+
     def LeftButtonRelease(self, obj, event):
         self.LeftButtonDown = False
+        rwi = self.GetInteractor()
+        rwi.Render()
 
     def Zoom(self):
         rwi = self.GetInteractor()
@@ -220,12 +231,170 @@ class BlenderStyle(vtk.vtkInteractorStyleUser):
 
             rwi.Render()
 
+    def InitializeScreenDrawing(self):
+        # make an image of the currently rendered image
+
+        rwi = self.GetInteractor()
+        rwin = rwi.GetRenderWindow()
+
+        size = rwin.GetSize()
+
+        self.PixelArray.Initialize()
+        self.PixelArray.SetNumberOfComponents(4)
+        self.PixelArray.SetNumberOfTuples(size[0] * size[1])
+
+        front = 1  # what does this do?
+        rwin.GetRGBACharPixelData(0, 0, size[0] - 1, size[1] - 1, front, self.PixelArray)
+
+
+    def DrawRubberBand(self):
+        rwi = self.GetInteractor()
+        rwin = rwi.GetRenderWindow()
+
+        x, y = rwi.GetEventPosition()
+
+        size = rwin.GetSize()
+
+        tempPA = vtk.vtkUnsignedCharArray()
+        tempPA.DeepCopy(self.PixelArray)
+
+        x = min(x, size[0]-1)
+        y = min(y, size[1]-1)
+
+        x = max(x, 0)
+        y = max(y, 0)
+
+        # Modify the pixel array
+        width = abs(x-self.start_x)
+        height = abs(y-self.start_y)
+        minx = min(x, self.start_x)
+        miny = min(y, self.start_y)
+
+        # draw top and bottom
+        for i in range(width):
+
+            c = round((10*i % 254)/254) * 254  # find some alternating color
+
+            id = (miny * size[0]) + minx + i
+            tempPA.SetTuple(id, (c,c,c,1))
+
+            id = ((miny+height) * size[0]) + minx + i
+            tempPA.SetTuple(id, (c, c, c, 1))
+
+        # draw left and right
+        for i in range(height):
+            c = round((10 * i % 254) / 254) * 254  # find some alternating color
+
+            id = ((miny + i) * size[0]) + minx
+            tempPA.SetTuple(id, (c, c, c, 1))
+
+            id = id + width
+            tempPA.SetTuple(id, (c, c, c, 1))
+
+
+        # and Copy back to the window
+        rwin.SetRGBACharPixelData(0, 0, size[0] - 1, size[1] - 1, tempPA, 0)
+        rwin.Frame()
+
+
+
+        #   this->Interactor->GetRenderWindow()->SetRGBACharPixelData(
+        #     0, 0, size[0] - 1, size[1] - 1, pixels, 0);
+        #   this->Interactor->GetRenderWindow()->Frame();
+
+
+        #
+        #   vtkUnsignedCharArray* tmpPixelArray = vtkUnsignedCharArray::New();
+        #   tmpPixelArray->DeepCopy(this->PixelArray);
+        #   unsigned char* pixels = tmpPixelArray->GetPointer(0);
+        #
+        #   int min[2], max[2];
+        #
+        #   min[0] =
+        #     this->StartPosition[0] <= this->EndPosition[0] ? this->StartPosition[0] : this->EndPosition[0];
+        #   if (min[0] < 0)
+        #   {
+        #     min[0] = 0;
+        #   }
+        #   if (min[0] >= size[0])
+        #   {
+        #     min[0] = size[0] - 1;
+        #   }
+        #
+        #   min[1] =
+        #     this->StartPosition[1] <= this->EndPosition[1] ? this->StartPosition[1] : this->EndPosition[1];
+        #   if (min[1] < 0)
+        #   {
+        #     min[1] = 0;
+        #   }
+        #   if (min[1] >= size[1])
+        #   {
+        #     min[1] = size[1] - 1;
+        #   }
+        #
+        #   max[0] =
+        #     this->EndPosition[0] > this->StartPosition[0] ? this->EndPosition[0] : this->StartPosition[0];
+        #   if (max[0] < 0)
+        #   {
+        #     max[0] = 0;
+        #   }
+        #   if (max[0] >= size[0])
+        #   {
+        #     max[0] = size[0] - 1;
+        #   }
+        #
+        #   max[1] =
+        #     this->EndPosition[1] > this->StartPosition[1] ? this->EndPosition[1] : this->StartPosition[1];
+        #   if (max[1] < 0)
+        #   {
+        #     max[1] = 0;
+        #   }
+        #   if (max[1] >= size[1])
+        #   {
+        #     max[1] = size[1] - 1;
+        #   }
+        #
+        #   int i;
+        #   for (i = min[0]; i <= max[0]; i++)
+        #   {
+        #     pixels[4 * (min[1] * size[0] + i)] = 255 ^ pixels[4 * (min[1] * size[0] + i)];
+        #     pixels[4 * (min[1] * size[0] + i) + 1] = 255 ^ pixels[4 * (min[1] * size[0] + i) + 1];
+        #     pixels[4 * (min[1] * size[0] + i) + 2] = 255 ^ pixels[4 * (min[1] * size[0] + i) + 2];
+        #     pixels[4 * (max[1] * size[0] + i)] = 255 ^ pixels[4 * (max[1] * size[0] + i)];
+        #     pixels[4 * (max[1] * size[0] + i) + 1] = 255 ^ pixels[4 * (max[1] * size[0] + i) + 1];
+        #     pixels[4 * (max[1] * size[0] + i) + 2] = 255 ^ pixels[4 * (max[1] * size[0] + i) + 2];
+        #   }
+        #   for (i = min[1] + 1; i < max[1]; i++)
+        #   {
+        #     pixels[4 * (i * size[0] + min[0])] = 255 ^ pixels[4 * (i * size[0] + min[0])];
+        #     pixels[4 * (i * size[0] + min[0]) + 1] = 255 ^ pixels[4 * (i * size[0] + min[0]) + 1];
+        #     pixels[4 * (i * size[0] + min[0]) + 2] = 255 ^ pixels[4 * (i * size[0] + min[0]) + 2];
+        #     pixels[4 * (i * size[0] + max[0])] = 255 ^ pixels[4 * (i * size[0] + max[0])];
+        #     pixels[4 * (i * size[0] + max[0]) + 1] = 255 ^ pixels[4 * (i * size[0] + max[0]) + 1];
+        #     pixels[4 * (i * size[0] + max[0]) + 2] = 255 ^ pixels[4 * (i * size[0] + max[0]) + 2];
+        #   }
+        #
+        #   this->Interactor->GetRenderWindow()->SetRGBACharPixelData(
+        #     0, 0, size[0] - 1, size[1] - 1, pixels, 0);
+        #   this->Interactor->GetRenderWindow()->Frame();
+        #
+        #   tmpPixelArray->Delete();
+
+
+
+
     def __init__(self):
 
         self.mode = None
         self.MotionFactor = 10
         self.MouseWheelMotionFactor = 0.5
 
+        self.start_x = 0  # start of a drag
+        self.start_y = 0
+
+        self.PixelArray = vtk.vtkUnsignedCharArray() # holds an image of the renderer output at the start of a drawing event
+
+        self.LeftButtonDown = False
         self.MiddleButtonDown = False
 
         self.AddObserver(vtk.vtkCommand.RightButtonPressEvent, self.RightButtonPress)
@@ -234,8 +403,8 @@ class BlenderStyle(vtk.vtkInteractorStyleUser):
         self.AddObserver(vtk.vtkCommand.MiddleButtonReleaseEvent, self.MiddleButtonRelease )
         self.AddObserver(vtk.vtkCommand.MouseWheelForwardEvent, self.MouseWheelForward )
         self.AddObserver(vtk.vtkCommand.MouseWheelBackwardEvent, self.MouseWheelBackward )
-        self.AddObserver(vtk.vtkCommand.LeftButtonPressEvent, LeftButtonPress)
-        self.AddObserver(vtk.vtkCommand.LeftButtonReleaseEvent, LeftButtonRelease)
+        self.AddObserver(vtk.vtkCommand.LeftButtonPressEvent, self.LeftButtonPress)
+        self.AddObserver(vtk.vtkCommand.LeftButtonReleaseEvent, self.LeftButtonRelease)
         self.AddObserver(vtk.vtkCommand.MouseMoveEvent, self.MouseMove)
 
 
