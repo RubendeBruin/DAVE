e62db40a1c099cb0ce39ff4839f5f50e0d7609ce
Ruben de Bruin <rubendebruin@gmail.com>
2022-11-15 17:35:42 +0100

    - added clippy

--- src/DAVE/gui/widget_rigg_it_right.py
+++ src/DAVE/gui/widget_rigg_it_right.py
@@ -5,6 +5,20 @@
 
   Ruben de Bruin - 2019
 """
+from PySide2.QtGui import QIcon
+
+"""
+The purpose of this widget is to give the user a quick way to do things. A bit like clippy used to be. But then
+one that actually works.
+
+How does it work
+Depending on the selected nodes (may be none) the agent makes and presents a list of logical next actions.
+It does this by calling all registered functions after any selection change.
+
+The functions get called with the current selection as argument and return a list or tuple of QtWidgets (usually buttons).
+The list may be empty. 
+
+"""
 
 from DAVE.gui.dockwidget import *
 from PySide2 import QtGui, QtCore, QtWidgets
@@ -15,6 +29,257 @@ import DAVE.settings as ds
 from DAVE.gui.forms.widget_selection_actions import Ui_SelectionActions
 from DAVE.gui.helpers.flow_layout import FlowLayout
 from DAVE.gui.helpers.popup_textbox import get_text
+
+from DAVE.settings import QUICK_ACTION_REGISTER
+
+def nodes_of_type(node_list, types):
+    return [node for node in node_list if isinstance(node, types)]
+
+def get_parent_code(selection):
+    frames = nodes_of_type(selection, Frame)
+    if frames:
+        return f", parent = '{frames[0].name}'"
+    else:
+
+        for node in selection:
+            current = node
+            while True:
+                parent = getattr(current,'parent',None)
+                if parent is None:
+                    break
+                else:
+                    if isinstance(parent, Frame):
+                        return f", parent = '{parent.name}'"
+                    current = parent
+
+        return ''
+
+def find_nodes(nodes, types):
+    """Returns nodes of given types. Only the first node per given type is returned and no duplicates.
+
+    Returns None if not all types could be found
+    """
+    r = []
+    source = nodes.copy()
+
+    for t in types:
+        for s in source:
+            if isinstance(s, t):
+                r.append(s)
+                source.remove(s)
+                break
+
+    if len(r) == len(types):
+        return r
+    else:
+        return None
+
+def point(scene, selection, *args):
+    code = f"s.new_point(name = '{scene.available_name_like('Point')}' {get_parent_code(selection)})"
+    btn = QPushButton('+ Point')
+    btn.setIcon(QIcon(":/icons/poi.png"))
+    return [(btn, code)]
+QUICK_ACTION_REGISTER.append(point)
+
+def frame(scene, selection, *args):
+    code = f"s.new_frame(name = '{scene.available_name_like('Frame')}' {get_parent_code(selection)})"
+    btn = QPushButton('+ Frame')
+    btn.setIcon(QIcon(":/icons/axis.png"))
+    return [(btn, code)]
+QUICK_ACTION_REGISTER.append(frame)
+
+def rigidbody(scene, selection, *args):
+    code = f"s.new_rigidbody(name = '{scene.available_name_like('Body')}' {get_parent_code(selection)})"
+    btn = QPushButton('+ RigidBody')
+    btn.setIcon(QIcon(":/icons/cube.png"))
+    return [(btn, code)]
+QUICK_ACTION_REGISTER.append(rigidbody)
+
+def circle(scene, selection, *args):
+    nodes = nodes_of_type(selection,Point)
+    if nodes:
+        code = f"s.new_circle(name = '{scene.available_name_like('Circle')}', parent = '{nodes[0].name}', axis = (0,1,0), radius = 1)"
+        btn = QPushButton('+ Circle')
+        btn.setIcon(QIcon(":/icons/circle.png"))
+        return [(btn, code)]
+
+    code = f"point = s.new_point(name = '{scene.available_name_like('Point')}' {get_parent_code(selection)})"
+    code += f"\ns.new_circle(name = '{scene.available_name_like('Circle')}', parent = point, axis = (0,1,0), radius = 1)"
+    btn = QPushButton('+ Point && Circle')
+    btn.setIcon(QIcon(":/icons/circle.png"))
+    return [(btn, code)]
+
+QUICK_ACTION_REGISTER.append(circle)
+
+def actions_on_circles(scene, selection, *args):
+    # At least two sheaves selected
+    style_warning = "color: darkred"
+
+    actions = []
+    p2 = find_nodes(selection, [nodes.Circle, nodes.Circle])
+
+    if p2:
+
+
+        p0 = p2[0]
+        p1 = p2[1]
+
+        # Grommet
+        button = QPushButton('+ Grommet')
+        button.setIcon(QIcon(":/icons/circle.png"))
+        name = scene.available_name_like("Grommet")
+        code = f's.new_cable("{name}", endA="{p0.name}", endB = "{p0.name}", sheaves = ["{p1.name}"])'
+
+        actions.append((button, code))
+
+        # Geometric contact between p0 -> p1
+        if p0.parent.parent is not None:
+
+            button = QPushButton(f'Connect using {p0.name} to {p1.name}')
+            button.setIcon(QIcon(':/icons/pin_hole.png'))
+            name = scene.available_name_like("Contact")
+            code = f's.new_geometriccontact("{name}", "{p0.name}", "{p1.name}")'
+
+            #  sheave poi   axis
+            if p0.parent.parent.parent is not None:
+                button.setStyleSheet(style_warning)
+                button.setToolTip(f"Warning: {p0.parent.parent.parent.name} will be disconnected from its parent")
+
+            actions.append((button, code))
+
+        # Geometric contact between p1 -> p0
+        if p1.parent.parent is not None:
+
+            button = QPushButton(f'Connect using {p1.name} to {p0.name}')
+            button.setIcon(QIcon(':/icons/pin_hole.png'))
+            name = scene.available_name_like("Contact")
+            code = f's.new_geometriccontact("{name}", "{p1.name}", "{p0.name}")'
+
+            #  sheave poi   axis
+            if p1.parent.parent.parent is not None:
+                button.setStyleSheet(style_warning)
+                button.setToolTip(
+                    f"Warning: {p1.parent.parent.parent.name} will be disconnected from its parent")
+
+            actions.append((button, code))
+
+    return actions
+
+QUICK_ACTION_REGISTER.append(actions_on_circles)
+
+def cables_slings_and_grommets(scene, selection, *args):
+
+    # Multiple points/sheaves selected
+    actions = []
+    poi_and_sheave = nodes_of_type(selection, (Point, Circle))
+    if poi_and_sheave:
+
+        # Cable
+        if len(poi_and_sheave) > 1:
+            button = QPushButton('+ Cable')
+            button.setIcon(QIcon(':/icons/cable.png'))
+
+            if len(poi_and_sheave) > 2:
+                names = ''.join([f'"{e.name}",' for e in poi_and_sheave[1:-1]])
+                sheaves = f', sheaves = [{names[:-1]}]'
+            else:
+                sheaves = ''
+
+            name = scene.available_name_like("Cable")
+            cable_code = f's.new_cable("{name}", endA="{poi_and_sheave[0].name}", endB = "{poi_and_sheave[-1].name}"{sheaves})'
+
+            actions.append((button, cable_code))
+
+        # Sling
+        if len(poi_and_sheave) > 1:
+            button = QPushButton('+ Sling')
+            button.setIcon(QIcon(':/icons/sling.png'))
+
+            if len(poi_and_sheave) > 2:
+                names = ''.join([f'"{e.name}",' for e in poi_and_sheave[1:-1]])
+                sheaves = f', sheaves = [{names[:-1]}]'
+            else:
+                sheaves = ''
+
+            name = scene.available_name_like("Sling")
+            cable_code = f's.new_sling("{name}", endA="{poi_and_sheave[0].name}", endB = "{poi_and_sheave[-1].name}"{sheaves})'
+
+            actions.append((button, cable_code))
+    return actions
+
+QUICK_ACTION_REGISTER.append(cables_slings_and_grommets)
+
+
+
+
+def con6d(scene, selection, *args):
+    frames = nodes_of_type(selection, Frame)
+    if len(frames) > 1:
+        f1 = frames[0]
+        f2 = frames[1]
+        name = scene.available_name_like(f'Con6d_{f1.name}_to_{f2.name}')
+        code = f"s.new_linear_connector_6d(name = '{name}', main = '{f2.name}', secondary = '{f1.name}')"
+
+        button = QPushButton('Connect with 6d connector')
+        button.setIcon(QIcon(':/icons/lincon6.png'))
+        return [(button, code)]
+    return []
+
+
+def connectors(scene, selection, *args):
+
+    actions = []
+    frames = nodes_of_type(selection, Frame)
+    if len(frames) > 1:
+        f1 = frames[0]
+        f2 = frames[1]
+
+        # Beam
+        name = scene.available_name_like(f'Beam')
+        code = f"s.new_beam(name = '{name}', nodeA = '{f1.name}', nodeB = '{f2.name}')"
+
+        button = QPushButton('Connect with beam')
+        button.setIcon(QIcon(':/icons/beam.png'))
+
+        actions.append((button, code))
+
+        # 2D
+        name = scene.available_name_like(f'Con2d_{f1.name}_to_{f2.name}')
+        code = f"s.new_connector2d(name = '{name}', nodeA = '{f2.name}', nodeB = '{f1.name}')"
+
+        button = QPushButton('Connect with 2d connector')
+        button.setIcon(QIcon(':/icons/con2d.png'))
+
+        actions.append((button, code))
+
+        # 6D
+        name = scene.available_name_like(f'Con6d_{f1.name}_to_{f2.name}')
+        code = f"s.new_linear_connector_6d(name = '{name}', main = '{f2.name}', secondary = '{f1.name}')"
+
+        button = QPushButton('Connect with 6d connector')
+        button.setIcon(QIcon(':/icons/lincon6.png'))
+        actions.append((button, code))
+
+    return actions
+
+QUICK_ACTION_REGISTER.append(connectors)
+
+def visual(scene, selection, *args):
+    parent = get_parent_code(selection)
+    if parent:
+        name = scene.available_name_like(f'Visual')
+        code = f"s.new_visual(name = '{name}' {parent}, path = 'res: cube_with_bevel.obj')"
+
+        button = QPushButton('+ Visual')
+        button.setIcon(QIcon(':/icons/visual.png'))
+        return [(button, code)]
+    return []
+
+QUICK_ACTION_REGISTER.append(visual)
+
+
+
+
 
 class WidgetQuickActions(guiDockWidget):
 
@@ -60,20 +325,9 @@ class WidgetQuickActions(guiDockWidget):
 
         Returns None if not all types could be found
         """
-        r = []
-        source = self.guiSelection.copy()
 
-        for t in types:
-            for s in source:
-                if isinstance(s, t):
-                    r.append(s)
-                    source.remove(s)
-                    break
+        return find_nodes(self.guiSelection, types)
 
-        if len(r) == len(types):
-            return r
-        else:
-            return None
 
     def all_of_type(self, types):
         source = self.guiSelection.copy()
@@ -102,6 +356,16 @@ class WidgetQuickActions(guiDockWidget):
 
         self.buttons.clear()
 
+        for qa in QUICK_ACTION_REGISTER:
+            actions = qa(self.guiScene, self.guiSelection)
+            for action in actions:
+                btn = action[0]
+                code = action[1]
+                self.buttons.append(btn)
+                btn.pressed.connect(lambda c=code, *args: self.guiRunCodeCallback(c, guiEventType.MODEL_STRUCTURE_CHANGED))
+
+
+
         style_warning = "color: darkred"
 
         # Here we manually define all the possible quick-actions
@@ -111,113 +375,7 @@ class WidgetQuickActions(guiDockWidget):
         def valid_name(text):
             return self.guiScene.name_available(text)
 
-        # At least two sheaves selected
-        p2 = self.find_nodes([nodes.Circle, nodes.Circle])
-        if p2:
 
-            p0 = p2[0]
-            p1 = p2[1]
-
-            # Grommet
-            button = QPushButton('Create grommet', self.ui.frame)
-
-            def create_grommet():
-                pos = self.mapToGlobal(QPoint(0, 0))
-                name = get_text(pos=pos, suggestion=self.guiScene.available_name_like("grommet"),
-                                input_valid_callback=valid_name)
-                sling_code = f's.new_cable("{name}", endA="{p0.name}", endB = "{p0.name}", sheaves = ["{p1.name}"])'
-                self.guiRunCodeCallback(sling_code, guiEventType.MODEL_STRUCTURE_CHANGED)
-
-            button.clicked.connect(create_grommet)
-            self.buttons.append(button)
-
-
-
-
-            # Geometric contact between p0 -> p1
-            if p0.parent.parent is not None:
-
-                button = QPushButton(f'Connection using {p1.name} as master', self.ui.frame)
-                def geometric_contact1():
-                    pos = self.mapToGlobal(QPoint(0,0))
-                    name = get_text(pos = pos, suggestion=self.guiScene.available_name_like("contact"), input_valid_callback=valid_name)
-                    pin_hole_code_p0p1 = f's.new_geometriccontact("{name}", "{p0.name}", "{p1.name}")'
-                    self.guiRunCodeCallback(pin_hole_code_p0p1, guiEventType.MODEL_STRUCTURE_CHANGED)
-
-                button.clicked.connect(geometric_contact1)
-
-                #  sheave poi   axis
-                if p0.parent.parent.parent is not None:
-                    button.setStyleSheet(style_warning)
-                    button.setToolTip(f"Warning: {p0.parent.parent.parent.name} will be disconnected from its parent")
-
-                self.buttons.append(button)
-
-            # Geometric contact between p1 -> p0
-            if p1.parent.parent is not None:
-
-                button = QPushButton(f'Connect using {p0.name} as master', self.ui.frame)
-
-                def geometric_contact2():
-                    pos = self.mapToGlobal(QPoint(0,0))
-                    name = get_text(pos = pos, suggestion=self.guiScene.available_name_like("contact"), input_valid_callback=valid_name)
-                    pin_hole_codep1p0 = f's.new_geometriccontact("{name}", "{p1.name}", "{p0.name}")'
-                    self.guiRunCodeCallback(pin_hole_codep1p0, guiEventType.MODEL_STRUCTURE_CHANGED)
-
-                button.clicked.connect(geometric_contact2)
-
-                #  sheave poi   axis
-                if p1.parent.parent.parent is not None:
-                    button.setStyleSheet(style_warning)
-                    button.setToolTip(
-                        f"Warning: {p1.parent.parent.parent.name} will be disconnected from its parent")
-
-                self.buttons.append(button)
-
-        # Multiple points/sheaves selected
-        poi_and_sheave = self.all_of_type([Point, Circle])
-        if poi_and_sheave:
-
-            if len(poi_and_sheave) > 1:
-                if len(poi_and_sheave) > 2:
-                    names = ''.join([f'"{e.name}",' for e in poi_and_sheave[1:-1]])
-                    sheaves = f', sheaves = [{names[:-1]}]'
-                    button = QPushButton('Create cable with sheaves', self.ui.frame)
-                else:
-                    sheaves = ''
-                    button = QPushButton('Create cable', self.ui.frame)
-
-                def create_cable():
-                    pos = self.mapToGlobal(QPoint(0,0))
-                    name = get_text(pos = pos, suggestion=self.guiScene.available_name_like("cable"), input_valid_callback=valid_name)
-                    cable_code = f's.new_cable("{name}", endA="{poi_and_sheave[0].name}", endB = "{poi_and_sheave[-1].name}"{sheaves})'
-                    self.guiRunCodeCallback(cable_code, guiEventType.MODEL_STRUCTURE_CHANGED)
-
-                button.clicked.connect(create_cable)
-                self.buttons.append(button)
-
-        # creating sling between points and sheaves
-        poi_and_sheave = self.all_of_type([Point, Circle])
-        if poi_and_sheave:
-
-            if len(poi_and_sheave) > 1:
-                if len(poi_and_sheave) > 2:
-                    names = ''.join([f'"{e.name}",' for e in poi_and_sheave[1:-1]])
-                    sheaves = f', sheaves = [{names[:-1]}]'
-                    button = QPushButton('Create sling over sheaves', self.ui.frame)
-                else:
-                    sheaves = ''
-                    button = QPushButton('Create sling', self.ui.frame)
-
-                def create_sling():
-                    pos = self.mapToGlobal(QPoint(0, 0))
-                    name = get_text(pos=pos, suggestion=self.guiScene.available_name_like("sling"),
-                                    input_valid_callback=valid_name)
-                    sling_code = f's.new_sling("{name}", endA="{poi_and_sheave[0].name}", endB = "{poi_and_sheave[-1].name}"{sheaves})'
-                    self.guiRunCodeCallback(sling_code, guiEventType.MODEL_STRUCTURE_CHANGED)
-
-                button.clicked.connect(create_sling)
-                self.buttons.append(button)
 
         # a single sheave selected
         if len(self.guiSelection)==1:
