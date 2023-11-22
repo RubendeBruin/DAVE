5281980892fd52e4fb33475565faa2e61187a52e
Ruben de Bruin <34062862+RubendeBruin@users.noreply.github.com>
2023-11-22 20:46:01 +0100

    Fixed 143 Added Node-added event

--- tests/test_ask_nicely.py
+++ tests/test_ask_nicely.py
@@ -22,9 +22,10 @@ def test_ask_nicely_gui():
     s.new_point("point")
 
     from PySide6.QtWidgets import QApplication
-    app = QApplication([])
+    if QApplication.instance() is None:
+        app = QApplication([])
 
     from DAVE import gui_globals
     gui_globals.do_ask_user_for_unavailable_nodenames = True
 
-    s['Point']
+    s['Point']  #<--- trigger the gui
