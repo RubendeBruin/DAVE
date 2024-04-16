64776ffc4d589186dbb443f0c2c50b92846f4a42
Ruben de Bruin <34062862+RubendeBruin@users.noreply.github.com>
2024-04-16 09:25:57 +0200

    fixed #159

--- tests/test_visuals/test_remove_cob_visual_when_no_displacement.py
+++ tests/test_visuals/test_remove_cob_visual_when_no_displacement.py
@@ -1,4 +1,7 @@
 from DAVE import *
+from DAVE.visual_helpers.simple_scene_renderer import SimpleSceneRenderer
+
+
 def model():
     s = Scene()
 
@@ -14,13 +17,26 @@ def model():
                     )
 
     # code for Tank
-    mesh = s.new_buoyancy(name='Tank',
+    mesh = s.new_buoyancy(name='Shape',
                       parent='Frame')
     mesh.trimesh.load_file(r'res: cube.obj', scale=(10.0, 10.0, 10.0), rotation=(0.0, 0.0, 0.0), offset=(0.0, 0.0, 0.0))
-    t = s['Tank']
+    t = s['Shape']
 
     return s, f, t
 
-if __name__ == '__main__':
-    s,f,t = model()
-    DG(s, autosave=False)
+def test_remove_cob_visual_when_no_displacement():
+    s, f, t = model()
+
+
+    renderer = SimpleSceneRenderer(s)
+
+    # get the cob visual
+    cob_visual = renderer.actor_from_node(t).actors['cob']
+    assert cob_visual.GetVisibility()
+
+    f.z = 20
+
+    renderer.position_visuals()
+    cob_visual = renderer.actor_from_node(t).actors['cob']
+    assert not cob_visual.GetVisibility()
+
