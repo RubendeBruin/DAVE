0166cebf5d9bde98ae477975711b1d22b88c92d7
Ruben de Bruin <34062862+RubendeBruin@users.noreply.github.com>
2024-09-26 15:46:47 +0200

    ignore errors in files when making thumbnails

--- src/DAVE/gui/thumbnailer/dave_thumbnails.py
+++ src/DAVE/gui/thumbnailer/dave_thumbnails.py
@@ -7,16 +7,20 @@ from DAVE.settings_visuals import PAINTERS
 from DAVE.visual_helpers.image_screen_renderer import ImageRenderer
 
 
-def give_pixmap_using_callback(file : Path, loader_func, resource_provider = None):
+def give_pixmap_using_callback(file: Path, loader_func, resource_provider=None):
     s = Scene()
     if resource_provider is not None:
         s.resource_provider = resource_provider
 
-    loader_func(s, file)
+    try:
+        loader_func(s, file)
+    except:
+        pass
 
     return _scene_to_pixmap(s)
 
-def give_pixmap_from_DAVE_model(file : Path, resource_provider = None):
+
+def give_pixmap_from_DAVE_model(file: Path, resource_provider=None):
     s = Scene()
     if resource_provider is not None:
         s.resource_provider = resource_provider
@@ -24,7 +28,8 @@ def give_pixmap_from_DAVE_model(file : Path, resource_provider = None):
     s.load(file, allow_error_during_load=True)
     return _scene_to_pixmap(s)
 
-def _scene_to_pixmap(s : Scene):
+
+def _scene_to_pixmap(s: Scene):
 
     ren = ImageRenderer(s)
     ren.settings.show_sea = False
@@ -39,5 +44,3 @@ def _scene_to_pixmap(s : Scene):
 
     image = pil_image.toqpixmap()
     return image
-
-
